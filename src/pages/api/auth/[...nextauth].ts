0aaf6b7bd19537a92a2f2f1e9bc386d978dc7d60
rhy3h <rhy3htw@gmail.com>
2023-04-24 23:51:23 +0800

    feat: Add third-party login via Discord

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+// https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+const scopes = ["identify"].join(" ");
+
+export default NextAuth({
+  providers: [
+    DiscordProvider({
+      clientId: process.env.MAGICORD_CLIENT_ID as string,
+      clientSecret: process.env.MAGICORD_CLIENT_SECRET as string,
+      authorization: { params: { scope: scopes } },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.accessToken = token.accessToken as string;
+      return session;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token as string;
+      }
+      return token;
+    },
+  },
+});
